4d776055df77ac53b8f263cf04528c8b0511a6e9
parkgun6 <a01088807068@gmail.com>
2022-04-17 00:53:51 +0900

    feat: Alert 기능 테스트 및 생성

--- src/pages/Wordle/index.tsx
+++ src/pages/Wordle/index.tsx
@@ -1,5 +1,28 @@
+import { useState } from 'react';
+import AlertPortal, { Alert } from '../../components/Alert';
+import { ALERT_MESSAGE } from '../../constants';
+
 const index = () => {
-  return <h1>Hello Wordle !! </h1>;
+  const [showAlert, setShowAlert] = useState(false);
+  const [message, setMessage] = useState('');
+  const onClickAlert = () => {
+    setMessage(ALERT_MESSAGE.CORRECT);
+    setShowAlert(true);
+    setTimeout(() => {
+      setShowAlert(false);
+    }, 2990);
+  };
+  return (
+    <>
+      <h1>Hello Wordle !! </h1>
+      <button onClick={onClickAlert}>정답 ALERT 테스트</button>
+      {showAlert && (
+        <AlertPortal>
+          <Alert message={message} />
+        </AlertPortal>
+      )}
+    </>
+  );
 };
 
 export default index;
