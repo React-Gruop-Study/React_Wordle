ed30a9b580f62e9aaf17f743420a77c80b7741de
parkgun6 <a01088807068@gmail.com>
2022-04-17 00:52:38 +0900

    style: Alert style ts파일 생성

---
+++ src/components/Alert/index.style.ts
@@ -0,0 +1,27 @@
+import styled from '@emotion/styled';
+
+export const AlertWithoutButton = styled.p`
+  position: absolute;
+  top: 15%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: gray;
+  color: white;
+  padding: 20px;
+  border-radius: 8px;
+  font-size: 1rem;
+  font-weight: bold;
+  animation: fade 3s linear;
+
+  @keyframes fade {
+    0% {
+      opacity: 0;
+    }
+    50% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 0;
+    }
+  }
+`;
