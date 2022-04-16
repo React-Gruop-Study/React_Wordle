a366883cf0982826e5133ed7f5c0e4a708461b4d
parkgun6 <a01088807068@gmail.com>
2022-04-17 00:52:52 +0900

    feat: createPortal을 이용한 Alert 구현

---
+++ src/components/Alert/index.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from 'react';
+import { createPortal } from 'react-dom';
+import { AlertWithoutButton } from './index.style';
+
+interface AlertProps {
+  message: string;
+}
+
+interface AlertPortalProps {
+  children: ReactNode;
+}
+
+const alertRoot = document.createElement('div');
+alertRoot.setAttribute('id', 'alert');
+document.body.appendChild(alertRoot);
+
+export const Alert = ({ message }: AlertProps) => {
+  return <AlertWithoutButton>{message}</AlertWithoutButton>;
+};
+
+const AlertPortal = ({ children }: AlertPortalProps) => {
+  return createPortal(children, alertRoot);
+};
+
+export default AlertPortal;
