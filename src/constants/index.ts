fa10d2a01b36cacdb91141a0399f95eed591dcce
parkgun6 <a01088807068@gmail.com>
2022-04-18 15:11:02 +0900

    feat: keyArray 및 기타 상수 생성

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,15 @@
+export const keyboardArray = [
+  ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+  ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+  ['ENTER', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '(X)'],
+];
+
 export const ALERT_MESSAGE = {
   CORRECT: '정답입니다 !!',
+  WORNG: '오답입니다 ㅠㅠ',
+  NOT_ENOUGH_LENGTH: '길이가 모자랍니다.',
 };
+
+export const WORD_MAX_LENGTH = 5;
+
+export const INCORRECT = 'APPLE';
