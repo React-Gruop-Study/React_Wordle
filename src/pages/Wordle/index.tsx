bfad90d741f584d0b638d1b22b0615661cdab980
parkgun6 <a01088807068@gmail.com>
2022-04-19 14:35:45 +0900

    feat: 키보드 클릭이벤트 적용

--- src/pages/Wordle/index.tsx
+++ src/pages/Wordle/index.tsx
@@ -9,6 +9,15 @@ import {
   WORD_MAX_LENGTH,
 } from '../../constants';
 import useCloseAlert from '../../hooks/useCloseAlert';
+import { BoxState } from '../../type';
+
+type CurrentRowCalculateResult = [
+  BoxState,
+  BoxState,
+  BoxState,
+  BoxState,
+  BoxState,
+];
 
 const index = () => {
   const [showAlert, setShowAlert] = useState(false);
@@ -20,32 +29,38 @@ const index = () => {
   const [word, setWord] = useState('');
   const [words, setWords] = useState<string[]>([]);
   const [alertMessage, setAlertMessage] = useState('');
+  const [correctIndex, setCorrectIndex] = useState<string[]>([]);
 
-  const onKeydwonEnter = () => {
+  const onKeydownEnter = () => {
     const wordsList = word.split('');
+    const questionList = INCORRECT.toUpperCase().split('');
     setWords(wordsList);
-
     if (word.length < WORD_MAX_LENGTH) {
       setAlertMessage(ALERT_MESSAGE.NOT_ENOUGH_LENGTH);
       setShowAlert(true);
       setWord('');
-    } else if (word == INCORRECT) {
+    } else if (word.toLowerCase() == INCORRECT) {
       setAlertMessage(ALERT_MESSAGE.CORRECT);
       setShowAlert(true);
     } else if (word !== INCORRECT) {
       setAlertMessage(ALERT_MESSAGE.WORNG);
       setShowAlert(true);
+      console.log('-----------------------------------');
+      wordsList.forEach((word, index) => {
+        word === questionList[index] &&
+          console.log(`${index + 1} 번 글자는 맞았어요`);
+        console.log(`${questionList.includes(word)} 는 들어있긴해여`);
+      });
+      console.log('-----------------------------------');
       setWord('');
     }
-    console.log('Enter........');
-    console.log(word);
   };
-  useEffect(() => {
-    console.log(words);
-  }, [words]);
+  // useEffect(() => {
+  //   console.log(words);
+  // }, [words]);
 
   const onKeydownWord = (key: string) => {
-    if (word.length >= 5) return;
+    if (word.length >= WORD_MAX_LENGTH) return;
 
     setWord((prev) => prev + key.toUpperCase());
     // console.log(`words : ${word}`);
@@ -54,12 +69,15 @@ const index = () => {
 
   const onKeydownBackspace = () => {
     if (!word) return;
-    setWord((prev): any => {
-      prev
-        .split('')
-        .slice(0, prev.length - 1)
-        .join('');
-    });
+    setWord(word.slice(0, -1));
+  };
+
+  const onClickKeyboard = (click: { target: HTMLInputElement }) => {
+    const clickWord = click.target.innerHTML;
+    if (clickWord === 'ENTER') return onKeydownEnter();
+    else if (clickWord === '(X)') return onKeydownBackspace();
+    else if (word.length >= WORD_MAX_LENGTH) return;
+    else setWord((prev) => prev + clickWord);
   };
 
   // const onClickAlert = () => {
@@ -84,8 +102,9 @@ const index = () => {
       <Board word={word} words={words} />
       <Keyboard
         onKeydownWord={onKeydownWord}
-        onKeydwonEnter={onKeydwonEnter}
+        onKeydownEnter={onKeydownEnter}
         onKeydownBackspace={onKeydownBackspace}
+        onClickKeyboard={onClickKeyboard}
       />
       {/* <div>
         <button onClick={onClickAlert}>정답 ALERT 테스트</button>
