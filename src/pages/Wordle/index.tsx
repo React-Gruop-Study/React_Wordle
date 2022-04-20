e004d7e4ee6b5441db323434426faa84f497c7e1
parkgun6 <a01088807068@gmail.com>
2022-04-20 16:49:49 +0900

    feat: Wrapper생성, 알고리즘 수정 및 입력값 정답상태 추가

--- src/pages/Wordle/index.tsx
+++ src/pages/Wordle/index.tsx
@@ -9,7 +9,8 @@ import {
   WORD_MAX_LENGTH,
 } from '../../constants';
 import useCloseAlert from '../../hooks/useCloseAlert';
-import { BoxState } from '../../type';
+import { BoardState, BoxState } from '../../type';
+import { Wrapper } from './index.style';
 
 type CurrentRowCalculateResult = [
   BoxState,
@@ -28,36 +29,64 @@ const index = () => {
 
   const [word, setWord] = useState('');
   const [words, setWords] = useState<string[]>([]);
+  const [rowState, setRowState] = useState<BoardState>([]);
   const [alertMessage, setAlertMessage] = useState('');
-  const [correctIndex, setCorrectIndex] = useState<string[]>([]);
+  const [recentWords, setRecentWords] = useState<string[]>([]);
 
   const onKeydownEnter = () => {
     const wordsList = word.split('');
     const questionList = INCORRECT.toUpperCase().split('');
     setWords(wordsList);
+    if (recentWords.length >= 5) {
+      setAlertMessage(INCORRECT.toUpperCase());
+      setShowAlert(true);
+      return;
+    }
+    const result: CurrentRowCalculateResult = [
+      'none',
+      'none',
+      'none',
+      'none',
+      'none',
+    ];
+    // 입력한 글자가 모자랄 때
     if (word.length < WORD_MAX_LENGTH) {
       setAlertMessage(ALERT_MESSAGE.NOT_ENOUGH_LENGTH);
       setShowAlert(true);
       setWord('');
+
+      // 입력한 글자가 정답일 때
     } else if (word.toLowerCase() == INCORRECT) {
       setAlertMessage(ALERT_MESSAGE.CORRECT);
       setShowAlert(true);
+      return;
+      // 입력한 글자가 오답일 때
     } else if (word !== INCORRECT) {
       setAlertMessage(ALERT_MESSAGE.WORNG);
       setShowAlert(true);
       console.log('-----------------------------------');
+      console.log(word);
       wordsList.forEach((word, index) => {
-        word === questionList[index] &&
-          console.log(`${index + 1} 번 글자는 맞았어요`);
-        console.log(`${questionList.includes(word)} 는 들어있긴해여`);
+        if (word === questionList[index]) {
+          console.log(`${index + 1} 번 ${word}글자는 맞았어요`);
+          result[index] = 'exact';
+        } else if (questionList.includes(word)) {
+          console.log(`${index + 1} 번 ${word}글자는 들어있긴해요`);
+          result[index] = 'close';
+        } else {
+          console.log(`${index + 1} 번 ${word}글자는 없어요`);
+        }
       });
       console.log('-----------------------------------');
+      setRecentWords((prev) => [...prev, word]);
       setWord('');
+      setRowState([...rowState, result]);
     }
   };
-  // useEffect(() => {
-  //   console.log(words);
-  // }, [words]);
+  useEffect(() => {
+    console.log(recentWords);
+    console.log(rowState);
+  }, [recentWords]);
 
   const onKeydownWord = (key: string) => {
     if (word.length >= WORD_MAX_LENGTH) return;
@@ -96,10 +125,15 @@ const index = () => {
   // }, [showAlert]);
 
   return (
-    <>
+    <Wrapper>
       <h1>Hello Wordle !! </h1>
       <h1>5글자를 입력해주세요. </h1>
-      <Board word={word} words={words} />
+      <Board
+        word={word}
+        words={words}
+        recentWords={recentWords}
+        rowState={rowState}
+      />
       <Keyboard
         onKeydownWord={onKeydownWord}
         onKeydownEnter={onKeydownEnter}
@@ -114,7 +148,7 @@ const index = () => {
           <Alert message={alertMessage} />
         </AlertPortal>
       )}
-    </>
+    </Wrapper>
   );
 };
 
