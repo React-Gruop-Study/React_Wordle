0b1ea2d82b49af222cf4a1d540d2714046adb3da
parkgun6 <a01088807068@gmail.com>
2022-04-20 16:48:56 +0900

    style: AlphabetBox style 생성

---
+++ src/components/AlphabetBox/index.style.tsx
@@ -0,0 +1,62 @@
+import { css, keyframes } from '@emotion/react';
+import styled from '@emotion/styled';
+import { BoxState } from '../../type';
+
+export const AlphabetBox = styled.div<{
+  boxState?: BoxState;
+}>`
+  width: 100%;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 2rem;
+  line-height: 2rem;
+  font-weight: bold;
+  vertical-align: middle;
+  box-sizing: border-box;
+  text-transform: uppercase;
+  user-select: none;
+  border: 2px solid #d3d6da;
+  &:before {
+    content: '';
+    padding-bottom: 100%;
+    display: inline-block;
+  }
+  ${({ boxState }) => boxState && boxStyle[boxState]}
+`;
+
+const boxStyle = {
+  exact: css`
+    background-color: #6aaa64;
+    border: 2px solid #6aaa64;
+    color: white;
+  `,
+  close: css`
+    background-color: #c9b458;
+    border: 2px solid #c9b458;
+    color: white;
+  `,
+  none: css`
+    background-color: #787c7e;
+    border: 2px solid #787c7e;
+    color: white;
+  `,
+};
+
+const pop = keyframes`
+    50% {
+      transform: scale(1.1);
+    }
+  `;
+
+const flip = keyframes`
+    0% {
+      transform: rotateX(0deg);
+    }
+    50% {
+      transform: rotateX(90deg);
+    }
+    100% {
+      transform: rotateX(0deg);
+    }
+  `;
