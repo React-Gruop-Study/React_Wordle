e0a0ebe09abfadf869d12588a8807dadc2195e3d
parkgun6 <a01088807068@gmail.com>
2022-04-21 12:29:13 +0900

    feat: checkWords로 검증함수 분리 콘솔로그 제거 리팩토링

--- src/pages/Wordle/index.tsx
+++ src/pages/Wordle/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import AlertPortal, { Alert } from '../../components/Alert';
 import Board from '../../components/Board';
 import Keyboard from '../../components/Keyboard';
@@ -6,20 +6,13 @@ import {
   ALERT_MESSAGE,
   EMPTY_ROW_MAX_LENGTH,
   INCORRECT,
-  keyboardArray,
   WORD_MAX_LENGTH,
 } from '../../constants';
 import useCloseAlert from '../../hooks/useCloseAlert';
 import { BoardState, BoxState } from '../../type';
 import { Wrapper } from './index.style';
 
-type CurrentRowCalculateResult = [
-  BoxState,
-  BoxState,
-  BoxState,
-  BoxState,
-  BoxState,
-];
+type WordInCorrectState = [BoxState, BoxState, BoxState, BoxState, BoxState];
 
 const index = () => {
   const [showAlert, setShowAlert] = useState(false);
@@ -29,72 +22,55 @@ const index = () => {
   }, [modalState]);
 
   const [word, setWord] = useState('');
-  const [words, setWords] = useState<string[]>([]);
   const [rowState, setRowState] = useState<BoardState>([]);
   const [alertMessage, setAlertMessage] = useState('');
   const [recentWords, setRecentWords] = useState<string[]>([]);
+  const [correctState, setCorrectState] = useState(false);
 
-  const onKeydownEnter = () => {
+  const checkWords = () => {
     const wordsList = word.split('');
     const questionList = INCORRECT.toUpperCase().split('');
-    setWords(wordsList);
-    if (recentWords.length >= EMPTY_ROW_MAX_LENGTH) {
-      setAlertMessage(INCORRECT.toUpperCase());
-      setShowAlert(true);
-      return;
-    }
-    const result: CurrentRowCalculateResult = [
-      'none',
-      'none',
-      'none',
-      'none',
-      'none',
-    ];
+    const result: WordInCorrectState = ['none', 'none', 'none', 'none', 'none'];
+    wordsList.forEach((alphabet, index) => {
+      console.log(alphabet);
+      console.log(questionList[index]);
+      if (alphabet === questionList[index]) {
+        result[index] = 'exact';
+      } else if (questionList.includes(alphabet)) {
+        result[index] = 'close';
+      }
+    });
+    setRowState([...rowState, result]);
+  };
+
+  const onKeydownEnter = () => {
+    if (correctState) return;
     // 입력한 글자가 모자랄 때
     if (word.length < WORD_MAX_LENGTH) {
       setAlertMessage(ALERT_MESSAGE.NOT_ENOUGH_LENGTH);
       setShowAlert(true);
-      setWord('');
-
-      // 입력한 글자가 정답일 때
-    } else if (word.toLowerCase() == INCORRECT) {
+      return;
+    }
+    checkWords();
+    // 끝까지 정답을 못맞췄을 경우
+    if (recentWords.length >= EMPTY_ROW_MAX_LENGTH) {
+      setCorrectState(true);
+      return;
+    }
+    setRecentWords((prev) => [...prev, word]);
+    setWord('');
+    if (word.toLowerCase() == INCORRECT) {
       setAlertMessage(ALERT_MESSAGE.CORRECT);
       setShowAlert(true);
+      setCorrectState(true);
       return;
-      // 입력한 글자가 오답일 때
-    } else if (word !== INCORRECT) {
-      setAlertMessage(ALERT_MESSAGE.WORNG);
-      setShowAlert(true);
-      console.log('-----------------------------------');
-      console.log(word);
-      wordsList.forEach((word, index) => {
-        if (word === questionList[index]) {
-          console.log(`${index + 1} 번 ${word}글자는 맞았어요`);
-          result[index] = 'exact';
-        } else if (questionList.includes(word)) {
-          console.log(`${index + 1} 번 ${word}글자는 들어있긴해요`);
-          result[index] = 'close';
-        } else {
-          console.log(`${index + 1} 번 ${word}글자는 없어요`);
-        }
-      });
-      console.log('-----------------------------------');
-      setRecentWords((prev) => [...prev, word]);
-      setWord('');
-      setRowState([...rowState, result]);
     }
   };
-  useEffect(() => {
-    console.log(recentWords);
-    console.log(rowState);
-  }, [recentWords]);
 
   const onKeydownWord = (key: string) => {
+    if (correctState) return;
     if (word.length >= WORD_MAX_LENGTH) return;
-
     setWord((prev) => prev + key.toUpperCase());
-    // console.log(`words : ${word}`);
-    // console.log(`words length : ${word.length}`);
   };
 
   const onKeydownBackspace = () => {
@@ -110,40 +86,19 @@ const index = () => {
     else setWord((prev) => prev + clickWord);
   };
 
-  // const onClickAlert = () => {
-  //   setAlertMessage(ALERT_MESSAGE.CORRECT);
-  //   setShowAlert(true);
-  // };
-
-  // useEffect(() => {
-  //   if (showAlert) {
-  //     const timeout = setTimeout(() => {
-  //       setShowAlert(false);
-  //     }, 1990);
-  //     // useEffect cleanup
-  //     return () => clearTimeout(timeout);
-  //   }
-  // }, [showAlert]);
-
   return (
     <Wrapper>
-      <h1>Hello Wordle !! </h1>
-      <h1>5글자를 입력해주세요. </h1>
-      <Board
-        word={word}
-        words={words}
-        recentWords={recentWords}
-        rowState={rowState}
-      />
+      <h1 style={{ textAlign: 'center' }}>WORDLE</h1>
+      <h1 style={{ textAlign: 'center' }}>
+        {correctState && `정답 : ${INCORRECT.toUpperCase()}`}
+      </h1>
+      <Board word={word} recentWords={recentWords} rowState={rowState} />
       <Keyboard
         onKeydownWord={onKeydownWord}
         onKeydownEnter={onKeydownEnter}
         onKeydownBackspace={onKeydownBackspace}
         onClickKeyboard={onClickKeyboard}
       />
-      {/* <div>
-        <button onClick={onClickAlert}>정답 ALERT 테스트</button>
-      </div> */}
       {showAlert && (
         <AlertPortal>
           <Alert message={alertMessage} />
